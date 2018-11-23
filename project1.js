15d1fd76f25a44f1bbabfcfa5e18c5382d7f6115
Ravinder68 <p.ravikumar6808@gmail.com>
2018-11-23 12:36:12 +0530

    first commit

---
+++ project1.js
@@ -0,0 +1,10 @@
+﻿//script.js
+var app = angular
+                .module("myApp",[])
+                .controller("myCont1",function($scope,$rootScope){
+                  $rootScope.rootScope="Iam the RootScope"; 
+                  $scope. name="";
+                })
+                .controller("myCont2",function($scope){
+                    $scope.name1="";
+                })
